d6af5d963ef32d9748fe1207a002753ee1c64e44
stefanikondic <imagine937@gmail.com>
2026-03-25 13:58:27 +0100

    more IC, fw and bw int

--- core/plotting.py
+++ core/plotting.py
@@ -2,17 +2,18 @@ import matplotlib.pyplot as plt
 
 
 def plot_vector_field(X, Y, U, V):
-    fig, ax = plt.subplots(figsize=(6, 6))
+    fig, ax = plt.subplots(figsize=(7, 7))
 
     ax.streamplot(X, Y, U, V, density=1.2)
 
     ax.set_xlabel("x")
     ax.set_ylabel("y")
     ax.set_title("Phase portrait")
+    ax.grid(True, alpha=0.3)
 
     return fig, ax
 
 
 def plot_trajectory(ax, x_traj, y_traj):
     ax.plot(x_traj, y_traj, linewidth=2)
-    ax.plot(x_traj[0], y_traj[0], "o")  # početna tačka
+    ax.plot(x_traj[0], y_traj[0], "o", markersize=6)
